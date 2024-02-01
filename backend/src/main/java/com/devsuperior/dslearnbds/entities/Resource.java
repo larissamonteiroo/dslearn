a4f789e7bfb4aa02420578fbe4a7b226a0fbba49
Larissa Monteiro <larissa.marques.1999@hotmail.com>
2024-01-31 22:12:32 -0300

    feat(section): Section Entity

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Resource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Resource.java
@@ -1,15 +1,11 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import com.devsuperior.dslearnbds.entities.enums.ResourceType;
 
@@ -30,6 +26,9 @@ public class Resource implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "offer_id")
 	private Offer offer;
+
+	@OneToMany(mappedBy = "resource")
+	private List<Section> sections = new ArrayList<>();
 
 	public Resource() {
 		super();
