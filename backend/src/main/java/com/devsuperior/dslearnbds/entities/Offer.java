1e175e49ec5221333af5d033e17852c7809ab5d2
Larissa Monteiro <larissa.marques.1999@hotmail.com>
2024-02-02 18:30:29 -0300

    fix(): missing associations, getters

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -30,7 +30,10 @@ public class Offer implements Serializable {
 
 	@OneToMany(mappedBy = "offer")
 	private List<Resource> resources = new ArrayList<>();
-	
+
+	@OneToMany(mappedBy = "offer")
+	private List<Topic> topics = new ArrayList<>();
+
 	public Offer() {
 		super();
 	}
@@ -86,6 +89,10 @@ public class Offer implements Serializable {
 
 	public List<Resource> getResources() {
 		return resources;
+	}
+
+	public List<Topic> getTopics() {
+		return topics;
 	}
 
 	@Override
